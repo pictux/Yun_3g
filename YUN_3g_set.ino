ffd4977a1a99b2a90a1a3763ed3ef59a0d42268b
pitusso <pictux@gmail.com>
2013-11-12 15:37:15 +0100

    Update #1

--- YUN_3g_set.ino
+++ YUN_3g_set.ino
@@ -1,7 +1,7 @@
 /*
-
-
-
+ Set 3G/UMTS env
+ created 11-2013
+ by Mirco Piccin
  */
 
 #include <Process.h>
@@ -10,31 +10,66 @@ void setup() {
   Bridge.begin();	// Initialize the Bridge
   Serial.begin(9600);	// Initialize the Serial
 
+  Process p;  
+
   // Wait until a Serial Monitor is connected.
   while(!Serial);
 
+  Serial.println("**Set 3G/UMTS environment**");
+  Serial.println();
+  Serial.println("Updating repository...");
 
-  Process p;  
   p.runShellCommand("/bin/opkg update");
-
+  while(p.running()); 
   while (p.available()>0) {
     char c = p.read();
     Serial.print(c);
   }  
+  
+  Serial.println();
+  Serial.println("Installing needed packages...");
 
-  p.runShellCommand("/bin/opkg install kmod-usb2 kmod-usb-core kmod-usb-serial kmod-usb-serial-option kmod-usb-serial-wwan usb-modeswitch usb-modeswitch-data libusb-1.0");
+  p.runShellCommand("/bin/opkg install unzip kmod-usb2 kmod-usb-core kmod-usb-serial kmod-usb-serial-option kmod-usb-serial-wwan usb-modeswitch usb-modeswitch-data libusb-1.0 comgt chat");
+  while(p.running()); 
+  while (p.available()>0) {
+    char c = p.read();
+    Serial.print(c);
+  }
+  
+  Serial.println();
+  Serial.println("Downloading & installing luci 3G conf page...");
 
+  p.runShellCommand("/usr/bin/curl -k https://raw.github.com/pictux/Yun_3g/master/luci-proto-3g_svn-r9931-1_ar71xx.ipk -o /tmp/luci-proto-3g_svn-r9931-1_ar71xx.ipk");
+  while(p.running()); 
   while (p.available()>0) {
     char c = p.read();
     Serial.print(c);
   }
 
+  p.runShellCommand("/bin/opkg install /tmp/luci-proto-3g_svn-r9931-1_ar71xx.ipk");
+  while(p.running()); 
+  while (p.available()>0) {
+    char c = p.read();
+    Serial.print(c);
+  }
+
+  Serial.println();
   Serial.println("***** ALL DONE *****");
+  Serial.println();
+  Serial.println("Rebooting...");
+  p.runShellCommand("/sbin/reboot");
+  
+  Serial.flush();
 }
 
 void loop() {
 
 }
+
+
+
+
+
 
 
 
